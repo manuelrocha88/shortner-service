278b7def9504bab249591f33487dddce1807af6f
Manuel Rocha <manuelrocha88@gmail.com>
2022-11-24 20:22:40 +0000

    Improve redirect logic

--- server/routes/LinkRedirect.js
+++ server/routes/LinkRedirect.js
@@ -4,12 +4,11 @@ const Link = require('../models/Link');
 const router = Router();
 
 router.get('/:urlPath', async (req, res) => {
-    const { urlPath } = req.params
-    console.log(urlPath);
+    const { urlPath } = req.params;
     try {
-        const link = await Link.find({ shortId: urlPath });
+        const link = await Link.findOne({ shortId: urlPath });
         if (!link) throw new Error('No Link found with this id');
-        res.status(200).redirect(link.redirectToUrl);
+        res.status(301).redirect(link.redirectToUrl);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
